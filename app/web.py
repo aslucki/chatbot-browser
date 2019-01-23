adce866a6fa9e593e210b19c872c3e172bf4c3fd
adam <adam.slucki@gmail.com>
2019-01-23 20:42:06 +0100

    Adding home route

--- app/web.py
+++ app/web.py
@@ -4,6 +4,10 @@ from flask import (Flask, render_template,
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/_check')
 def healthcheck():
     resp = jsonify(success=True)
