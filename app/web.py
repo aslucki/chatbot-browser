4916a4cdb411efa49c5d98b124f151dad395cef2
adam <adam.slucki@gmail.com>
2019-01-24 00:47:45 +0100

    Next fix

--- app/web.py
+++ app/web.py
@@ -6,6 +6,7 @@ from flask import (Flask, render_template,
                    redirect, url_for)
 
 app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
 
 
 @app.route('/_check')
@@ -39,19 +40,6 @@ def reset_command():
         f.write('alert("No action")')
 
     return jsonify(success=True)
-
-
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
 
 
 def extract_request_data(data_dict: dict) -> tuple:
