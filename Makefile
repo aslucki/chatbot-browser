be28ee090b79423ee82aa845d4896e138427e2a7
adam <adam.slucki@gmail.com>
2019-01-23 20:07:15 +0100

    Code for testing POST requests

--- Makefile
+++ Makefile
@@ -7,3 +7,8 @@ build:
 start_app:
 	docker run --rm -ti -p $(PORT):$(PORT) $(IMAGE_NAME) \
 		   gunicorn -b 0.0.0.0:$(PORT) web:app
+
+dev:
+	docker run --rm -ti -p $(PORT):$(PORT) \
+	       -v $(PWD)/app:/app $(IMAGE_NAME) \
+		   gunicorn -b 0.0.0.0:$(PORT) web:app
--- app/web.py
+++ app/web.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('home.html')
+
+@app.route('/chatbot', methods=['GET', 'POST'])
+def handle_fulfillment():
+    if request.method == 'POST':
+        return request.data
+    else:
+        return "received GET request"
